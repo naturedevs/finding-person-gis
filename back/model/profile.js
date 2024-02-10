97db1674a0fb732adee66924c10d20cea56e065a
FlyDev515 <142128771+whitesnowman515@users.noreply.github.com>
2024-02-10 08:21:09 +0800

    complete back step1

---
+++ back/model/profile.js
@@ -0,0 +1,21 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
